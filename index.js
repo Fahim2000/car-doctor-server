3607780603dd3c0f2e0c1a17369829345837c9be
Fahim2000 <fahim.193902022@green.ac.bd>
2024-05-09 18:03:33 +0600

    vercel configuration added

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
-
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -14,7 +13,7 @@ app.use(express.json());
 const uri =
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sdohfmu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
